685af44b06a8b66b495b7c534cbf56544106edf7
Sulverus <sulverus@gmail.com>
2016-10-27 23:11:58 +0300

    test-run updated, all tests updated with inspector usage

--- test/api/all.test.lua
+++ test/api/all.test.lua
@@ -1,8 +1,9 @@
---# create server master1 with script='api/master1.lua', lua_libs='api/lua/connpool.lua'
---# create server master2 with script='api/master2.lua', lua_libs='api/lua/connpool.lua'
---# start server master1
---# start server master2
---# set connection default
+env = require('test_run')
+test_run = env.new()
+test_run:cmd("create server master1 with script='api/master1.lua', lua_libs='api/lua/connpool.lua'")
+test_run:cmd("create server master2 with script='api/master2.lua', lua_libs='api/lua/connpool.lua'")
+test_run:cmd("start server master1")
+test_run:cmd("start server master2")
 pool:wait_connection()
 pool:wait_table_fill()
 
@@ -12,10 +13,8 @@ servers = pool:all()
 zone = pool:all('myzone1')
 #zone
 
---# stop server master1
---# cleanup server master1
---# stop server master2
---# cleanup server master2
---# stop server default
---# start server default
---# set connection default
+_ = test_run:cmd("stop server master1")
+_ = test_run:cmd("stop server master2")
+test_run:cmd("cleanup server master1")
+test_run:cmd("cleanup server master2")
+test_run:cmd("restart server default with cleanup=1")
--- test/api/node_down.test.lua
+++ test/api/node_down.test.lua
@@ -1,44 +1,43 @@
---# create server master1 with script='api/master1.lua', lua_libs='api/lua/connpool.lua'
---# create server master2 with script='api/master2.lua', lua_libs='api/lua/connpool.lua'
---# start server master1
---# start server master2
---# set connection default
+env = require('test_run')
+test_run = env.new()
+test_run:cmd("create server master1 with script='api/master1.lua', lua_libs='api/lua/connpool.lua'")
+test_run:cmd("create server master2 with script='api/master2.lua', lua_libs='api/lua/connpool.lua'")
+test_run:cmd("start server master1")
+test_run:cmd("start server master2")
 pool:wait_connection()
 --shard.wait_epoch(3)
 pool:wait_table_fill()
 pool:is_table_filled()
 
---# set connection master1
+test_run:cmd("switch master1")
 pool:wait_table_fill()
 pool:is_table_filled()
 
---# set connection master2
+test_run:cmd("switch master2")
 pool:wait_table_fill()
 pool:is_table_filled()
 
---# set connection default
+test_run:cmd("switch default")
 
 servers = pool:all()
 #servers
 
 -- Kill server and wait for monitoring fibers kill
---# stop server master1
+_ = test_run:cmd("stop server master1")
 
 -- Check that node is removed from shard
 pool:wait_epoch(2)
 pool:is_table_filled()
 
---# set connection master2
+test_run:cmd("switch master2")
 pool:wait_epoch(2)
 pool:is_table_filled()
---# set connection default
+test_run:cmd("switch default")
 
 servers = pool:all()
 #servers
 
---# stop server master2
---# cleanup server master1
---# cleanup server master2
---# stop server default
---# start server default
---# set connection default
+_ = test_run:cmd("stop server master2")
+test_run:cmd("cleanup server master1")
+test_run:cmd("cleanup server master2")
+test_run:cmd("restart server default with cleanup=1")
